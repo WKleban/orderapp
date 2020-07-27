39e3d670c5eaf1aa4b8b089feee15ae21425effc
WKleban <66591952+WKleban@users.noreply.github.com>
2020-07-27 15:24:58 +0200

    fake data

--- src/main/java/pl/wotu/orderapp/FTPConnectionData.java
+++ src/main/java/pl/wotu/orderapp/FTPConnectionData.java
@@ -1,6 +1,7 @@
 package pl.wotu.orderapp;
 
 public class FTPConnectionData {
+    // FAKE
     public static String server = "ftp.yourftpserveraddress.pl";
     public static int port = 21;
     public static String user = "user";
